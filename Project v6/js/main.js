5dfb909e6e87fa9ba53d558f3a4e5b932371872d
Kalenyuk A <frosmatic@gmail.com>
2018-11-13 15:49:43 +0200

    Project v7

---
+++ Project v6/js/main.js
@@ -0,0 +1,13 @@
+let startBtn = document.getElementById('start');
+
+
+
+let expensesItem = document.getElementsByClassName('expenses-item');
+
+let btn = document.getElementsByTagName('button');
+
+let expensesBtn = btn[0];
+let optExpensesBtn = btn[1];
+
+let optExpensesItem = document.querySelectorAll('.optionalexpenses-item');
+
